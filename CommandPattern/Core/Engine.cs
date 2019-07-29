74a9661a36029b0d01c6b438d8de7f971c93648b
Angel Hristov <angelxristov84@abv.bg>
2019-07-29 23:20:57 +0300

    Add project files.

---
+++ CommandPattern/Core/Engine.cs
@@ -0,0 +1,19 @@
+﻿using CommandPattern.Core.Contracts;
+
+namespace CommandPattern
+{
+    internal class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter command)
+        {
+            this.commandInterpreter = command;
+        }
+
+        public void Run()
+        {
+            System.Console.ReadLine();
+        }
+    }
+}
